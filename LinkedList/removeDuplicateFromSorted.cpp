4c9478cb26645629be8d17b36f1359aee24fb31e
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2022-02-21 17:15:40 +0530

    Approach2

--- LinkedList/removeDuplicateFromSorted.cpp
+++ LinkedList/removeDuplicateFromSorted.cpp
@@ -21,3 +21,30 @@ Node * uniqueSortedList(Node * head) {
     
     return head;// Write your code here.
 }
+
+
+// Approach2
+Node * removeDuplicates( Node *head) 
+    {
+        if(head==NULL){
+            return NULL;
+        }
+        Node* curr=head->next;
+        Node* prev=head;
+        map<int,bool> visited;
+        visited[head->data]=true;
+        while(curr!=NULL){
+            if(visited[curr->data]==true){
+                Node* next_next = curr ->next;
+                delete(curr);
+                prev -> next = next_next;
+                curr=next_next;
+            }
+            else{
+                visited[curr->data]=true;
+                curr=curr->next;
+                prev=prev->next;
+            }
+        }
+        return head;
+    }
